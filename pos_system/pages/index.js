1ffff814397656faf3c86d82a103e27171cc3d11
Aakash Haran <aakash.haran01@gmail.com>
2022-10-26 19:45:01 -0500

    added most of the components the home page

--- pos_system/pages/index.js
+++ pos_system/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/universal/Home.module.css';
 import NavBar from '../components/universal/NavBar'
 
 const navItems = [
@@ -13,7 +13,15 @@ export default function Home() {
   return (
     <div>
       <NavBar navItems={navItems} isCustomer={false} />
-      <h1>ENTRY POINT</h1>
+      <div className={styles.container}>
+        <div className={styles.sectionleft}>
+          <div className={styles.textcontainer}>
+            <h1 style={{ fontSize: '80px', fontFamily: 'serif' }}>Order our World Famous Chick-Fil-A Sandwich!</h1>
+            <a className={styles.orderbutton}>Order Now!</a>
+          </div>
+        </div>
+        <div className={styles.sectionright}></div>
+      </div>
     </div>
   );
 };
