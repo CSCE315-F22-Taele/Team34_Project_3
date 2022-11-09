15108b4f062214e7513fb0c034ec990c2fe2402c
Aakash Haran <aakash.haran01@gmail.com>
2022-11-08 22:59:30 -0600

    health api

--- pos_system/pages/api/hello.js
+++ pos_system/pages/api/hello.js
@@ -1,8 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const resObj = {};
-  for (var a = 1; a < 10; a++)
-    resObj[a] = a;
-  res.status(200).json(resObj);
+  res.status(200);
 }
