bac99c2aa4c3fd692145afa3eb26a15aa10c86bd
Aakash Haran <aakash.haran01@gmail.com>
2022-11-08 23:09:10 -0600

    health api

--- pos_system/.next/static/webpack/webpack.914c864d2fa8ea5f.hot-update.js
+++ pos_system/.next/static/webpack/webpack.914c864d2fa8ea5f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "cc36bc45d3803198"; }
+/******/ 	__webpack_require__.h = function() { return "2506217f41b13aa7"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
