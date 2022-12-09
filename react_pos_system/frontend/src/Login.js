fe3ede5371a36b50d28ee4c7f71b81d565727d96
Aakash Haran <aakash.haran01@gmail.com>
2022-12-09 00:55:31 -0600

    mini commit

--- react_pos_system/frontend/src/Login.js
+++ react_pos_system/frontend/src/Login.js
@@ -103,7 +103,7 @@ const Login = (props) => {
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email Address</label>
                         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <div id="emailHelp" class="form-text">Enter your employee assigned Chick-Fil-A email</div>
+                        <div id="emailHelp" class="form-text">Enter your employee assigned Chick-Fil-A email address</div>
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
